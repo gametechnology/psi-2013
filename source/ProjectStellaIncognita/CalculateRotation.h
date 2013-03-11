9e9cdaf11b87c4bf792519521c16dc7802ba3f65
Hugo Mater <mater09@hva.nl>
2013-03-11 12:25:18 +0100

    Made a yaw pitch roll function to calculate the right matrix for rotations.

---
+++ source/ProjectStellaIncognita/CalculateRotation.h
@@ -0,0 +1,24 @@
+#ifndef CALC_ROTATION_H
+#define CALC_ROTATION_H
+
+#include <irrlicht.h>
+#include <math.h>
+
+using namespace irr;
+using namespace core;
+using namespace scene;
+using namespace video;
+using namespace io;
+using namespace gui;
+
+class CalculateRotation
+{
+public:
+
+	static quaternion CreateFromYawPitchRoll(float yaw, float pitch, float roll);
+
+	static matrix4 CreateFromQuaternion(quaternion quat);
+
+};
+
+#endif
