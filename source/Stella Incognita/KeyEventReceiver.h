e52fde08419336c485be255b06a7bc96a705b4eb
Roy Springer <roy.springer@hva.nl>
2013-03-18 12:14:10 +0100

    Added KeyEventReceiver for porting

---
+++ source/Stella Incognita/KeyEventReceiver.h
@@ -0,0 +1,38 @@
+
+#include "stdafx.h"
+using namespace std;
+
+#include <irrlicht.h>
+
+using namespace irr;
+using namespace core;
+using namespace scene;
+using namespace video;
+using namespace io;
+using namespace gui;
+
+class KeyEventReceiver : public IEventReceiver {
+public:
+    // This is the one method that we have to implement
+    virtual bool OnEvent(const SEvent& event) {
+        // Remember whether each key is down or up
+        if (event.EventType == irr::EET_KEY_INPUT_EVENT)
+            KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
+
+        return false;
+    }
+
+    // This is used to check whether a key is being held down
+    virtual bool IsKeyDown(EKEY_CODE keyCode) const {
+        return KeyIsDown[keyCode];
+    }
+    
+    KeyEventReceiver() {
+        for (u32 i=0; i<KEY_KEY_CODES_COUNT; ++i)
+            KeyIsDown[i] = false;
+    }
+
+    // We use this array to store the current state of each key
+    bool KeyIsDown[KEY_KEY_CODES_COUNT];
+private:
+};
