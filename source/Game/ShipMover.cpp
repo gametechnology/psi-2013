369cbb5f034e6871e11054c3ef20fce91f201e33
Silvan Herrema <silvan.herrema@hva.nl>
2013-03-25 13:47:14 +0100

    Empty Shipmover Added. Component fixed

--- source/Game/ShipMover.cpp
+++ source/Game/ShipMover.cpp
@@ -1,11 +1,27 @@
-#include "ShipMover.h"
 
-ShipMover::ShipMover(Composite* parent) : Component(parent)
+#include "ShipMover.h"
+#include "Engine\Entity.h"
+
+ShipMover::ShipMover(Composite* parent, Thruster* thrusters[]) : Component(parent)
 {
 
 }
 
-ShipMover::~ShipMover()
+
+ShipMover::~ShipMover(void)
 {
+
+
+}
+
+void ShipMover::update(){
+
+}
+void ShipMover::init(){
+
+}
+
+void ShipMover::draw(){
+
 
 }
