102990435f32fc97f6dc267b1a89bd9f63a6074d
Roy Springer <roy.springer@hva.nl>
2013-03-25 11:30:06 +0100

    Set back Backup

--- source/Game/Skybox.h
+++ source/Game/Skybox.h
@@ -5,8 +5,8 @@
 #ifndef SKYBOX
 #define SKYBOX
 
-#include "Engine/Entity.h"
-#include "Engine/Game.h"
+#include "Entity.h"
+#include "Game.h"
 class Skybox : public Entity  {
 public:
 	Skybox(const io::path & filePath, Composite* parent);
