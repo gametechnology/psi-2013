30d54699b67d59ef4ad947c2e7a3f8c7b2e9c133
Bas Roding <bas.roding@gmail.com>
2013-03-25 13:14:25 +0100

    Added a sprite to the ShipmapScene

--- source/Game/ShipmapScene.cpp
+++ source/Game/ShipmapScene.cpp
@@ -1,4 +1,6 @@
 #include "ShipmapScene.h"
+#include "Sprite.h"
+#include "Engine/Game.h"
 
 ShipmapScene::ShipmapScene()
 {
@@ -10,4 +12,9 @@ ShipmapScene::~ShipmapScene()
 
 void ShipmapScene::init()
 {
+	Sprite* sprite = new Sprite(this);
+	sprite->setTexture(Game::driver->getTexture("../assets/shipmap/map.png"), rect<s32>(0, 0, 500, 500));
+	sprite->setPosition(position2d<s32>(0, 0));
+
+	addComponent(sprite);
 }
