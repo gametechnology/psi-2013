0fd761910c13f68f28fe26224ca6d0aba71bd826
Silvan Herrema <silvan.herrema@hva.nl>
2013-03-25 13:47:14 +0100

    Empty Shipmover Added. Component fixed

--- source/Game/Thruster.cpp
+++ source/Game/Thruster.cpp
@@ -1,11 +1,26 @@
 #include "Thruster.h"
+#include "Engine\Component.h"
 #include "Engine\Game.h"
+#include "Engine\Entity.h"
 
-Thruster::Thruster(Composite* parent, vector3df position, vector3df initialDirection) : Component(parent)
+Thruster::Thruster(Composite* parent, vector3df position, vector3df initialDirection) : Entity(parent)
 {
 
 }
 
 Thruster::~Thruster()
 {
+
+
+}
+
+void Thruster::update(){
+
+}
+void Thruster::init(){
+
+}
+
+void Thruster::draw(){
+
 }
