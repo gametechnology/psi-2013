9f47c22f543580da54e847ef2187a8721b9d81ce
Rik Ruiten <rik.ruiten@hva.nl>
2013-03-25 14:38:08 +0100

    Start of GalaxyMap.

---
+++ source/Game/GalaxyMap.cpp
@@ -0,0 +1,20 @@
+#include "GalaxyMap.h"
+
+GalaxyMap::GalaxyMap(Composite* parent):Entity(parent)
+{
+
+}
+
+void GalaxyMap::loadMap()
+{
+
+}
+void GalaxyMap::saveMap()
+{
+
+}
+
+GalaxyMap::~GalaxyMap()
+{
+
+}
---
+++ source/Game/GalaxyMap.h
@@ -0,0 +1,16 @@
+#ifndef GALAXYMAP
+#define GALAXYMAP
+
+#include "Engine/Entity.h"
+#include <list>
+class MapSector;
+class GalaxyMap : public Entity  {
+public:
+	GalaxyMap(Composite* parent);
+	virtual ~GalaxyMap();
+	void loadMap();
+	void saveMap();
+	std::list<MapSector*> connections;
+};
+
+#endif
