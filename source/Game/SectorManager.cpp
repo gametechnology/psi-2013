a1c111fb8371f09549c66b2a8b7e48ea6670a803
Stijn Groothuis <stijn.groothuis@hva.nl>
2013-03-25 13:38:55 +0100

    filled in next_sector message

--- source/Game/SectorManager.cpp
+++ source/Game/SectorManager.cpp
@@ -5,11 +5,15 @@ SectorManager::SectorManager(Composite* parent) : Entity(parent) {
 	
 }
 void SectorManager::init(){
-	_currentSector = new SectorTemplate(this->parent,0,"..\..\assets\Textures\SkyBoxes\skybox01.png",20.0);
+	_currentSector = new SectorTemplate(this->parent,"..\..\assets\Textures\SkyBoxes\skybox01.png",20.0);
+	SectorManager::addComponent(_currentSector);
 }
 void SectorManager::handleMessage(unsigned int message, void* data) {
 	switch(message) {
 		case NEXT_SECTOR: /* Switch Sector */
+			delete _currentSector;
+			_currentSector = new SectorTemplate(this->parent,"..\..\assets\Textures\SkyBoxes\skybox01.png",20.0);
+			SectorManager::addComponent(_currentSector);
 
 			break;
 	}
