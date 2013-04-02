056e6458ead19f9d0efafa9de84705b1bd50d460
Silvan Herrema <silvan.herrema@hva.nl>
2013-04-02 14:00:32 +0200

    Camera fix. Orientation / rotation

--- source/Engine/CameraMover.cpp
+++ source/Engine/CameraMover.cpp
@@ -3,12 +3,12 @@
 
 CameraMover::CameraMover(Camera* parent):Component(parent)
 {
-
+	cam = (Camera*)(parent);
+	
 }
 
 void CameraMover::update()
 {
-	Camera* cam = (Camera*)(parent);
 	cam->position = cam->node->getPosition();
 	cam->orientation = cam->node->getRotation();
 	if (cam->parentIsEntity)
@@ -18,6 +18,24 @@ void CameraMover::update()
 		cam->position += cam->getEntity()->position;
 		cam->orientation += cam->getEntity()->orientation;
 	}
+	oldParentPosition = cam->getEntity()->position;
+	oldParentOrientation = cam->getEntity()->orientation;
+
+
+	vector3df tempPosition = cam->position;
+	vector3df tempRotation = cam->orientation;
+
+	vector3df forward = vector3df(0,0,1);
+	vector3df right = vector3df(1,0,0);
+	vector3df up = vector3df(0,1,0);
+	matrix4 nodeRotation = matrix4().setRotationDegrees(tempRotation);
+	
+	nodeRotation.transformVect(forward);
+	nodeRotation.transformVect(right);
+	nodeRotation.transformVect(up);
+
+	cam->getCameraNode()->setTarget(cam->position + forward);
+	cam->getCameraNode()->setUpVector(cam->position +up);
 }
 
 CameraMover::~CameraMover()
