6e41c1246b3a0870d274df71121a70868d675303
Nathan van Geffen <nathan.van.geffen@hva.nl>
2013-04-03 10:52:45 +0200

    Changed header Asteroid

--- build/EnemyAsteroid.h
+++ build/EnemyAsteroid.h
@@ -1,8 +1,13 @@
-#pragma once
-class EnemyAsteroid
+#ifndef ENEMY_ASTEROID_H
+#define ENEMY_ASTEROID_H
+
+#include "Enemy.h"
+
+class EnemyAsteroid: public Enemy
 {
 public:
 	EnemyAsteroid(void);
 	~EnemyAsteroid(void);
 };
 
+#endif
