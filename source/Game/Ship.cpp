8260d2a076f9128dfac6449a836424410f854b3f
Simon Karman <simon.karman@hva.nl>
2013-04-04 15:52:26 +0200

    Added ship class that contains all the shipstations

--- source/Game/Ship.cpp
+++ source/Game/Ship.cpp
@@ -1,11 +1,53 @@
 #include "Ship.h"
+#include "Stations/Station.h"
 
-
-Ship::Ship(Composite * parent) : Entity(parent)
+Ship::Ship( Composite * parent ) : Entity ( parent )
 {
+	
 }
 
+Ship :: Ship( ): Entity ( NULL )
+{ 
+	//TODO remove temp stuff
+	this -> _powerStation		= new PowerStation( this );
+	this -> _defenceStation		= new DefenceStation( this );
+	this -> _helmStation		= new HelmStation( this );
+	this -> _navigationStation	= new NavigationStation( this );
+	this -> _weaponStation		= new WeaponStation( this );
+
+	this -> _defenceStation		-> Initialize();
+	this -> _helmStation		-> Initialize();
+	this -> _navigationStation	-> Initialize();
+	this -> _weaponStation		-> Initialize();
+	this -> _powerStation		-> Initialize();
+}
 
 Ship::~Ship(void)
 {
+
+}
+
+Station *Ship :: GetStation( STATION_TYPE s )
+{
+	this -> _defenceStation -> Damage( );
+
+	switch( s )
+	{
+	case STATION_TYPE :: Defence:
+		return this -> _defenceStation;
+		break;
+	case STATION_TYPE :: Helm:
+		return this -> _helmStation;
+		break;
+	case STATION_TYPE :: Navigation:
+		return this -> _navigationStation;
+		break;
+	case STATION_TYPE :: Power:
+		return this -> _powerStation;
+		break;
+	case STATION_TYPE :: Weapon:
+		return this -> _weaponStation;
+		break;
+	};
+	return NULL;
 }
