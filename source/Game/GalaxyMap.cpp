05d7be6951fd36f48100cb1f62b6497ef1195155
Steven Gunneweg <steven.gunneweg@hva.nl>
2013-04-04 13:40:46 +0200

    Added commented code to draw only the explored sectors and connections between explored sectors.

--- source/Game/GalaxyMap.cpp
+++ source/Game/GalaxyMap.cpp
@@ -26,15 +26,23 @@ GalaxyMap::~GalaxyMap()
 
 void GalaxyMap::draw()
 {
+	//TODO: uncomment code to make sectors and connections invissible if not visited yet
+
 	video::ITexture* bgMap = Game::driver->getTexture("../assets/galaxy.jpg");
 	Game::driver->draw2DImage(bgMap, rect<s32>(0,0,widthMap,heightMap), rect<s32>(0,0,bgMap->getOriginalSize().Width,bgMap->getOriginalSize().Height));
 
 	for(std::list<MapSector*>::iterator i = sectors.begin(); i != sectors.end(); ++i)
 	{
-		for(std::list<MapSector*>::iterator j = (*i)->connections.begin(); j != (*i)->connections.end(); ++j)
-		{
-			Game::driver->draw2DLine(vector2d<irr::s32>((*i)->position.X, (*i)->position.Y), vector2d<irr::s32>((*j)->position.X, (*j)->position.Y));
-		}
+		//if ((*i)->explored)
+		//{
+			for(std::list<MapSector*>::iterator j = (*i)->connections.begin(); j != (*i)->connections.end(); ++j)
+			{
+				//if ((*i)->explored && (*j)->explored)
+				//{
+					Game::driver->draw2DLine(vector2d<irr::s32>((*i)->position.X, (*i)->position.Y), vector2d<irr::s32>((*j)->position.X, (*j)->position.Y));
+				//}
+			}
+		//}
 	}
 	
 	gui::IGUIFont* font = Game::device->getGUIEnvironment()->getBuiltInFont();
@@ -42,8 +50,11 @@ void GalaxyMap::draw()
 	
 	for(std::list<MapSector*>::iterator i = sectors.begin(); i != sectors.end(); ++i)
 	{
-		(*i)->draw();
-		font->draw(irr::core::stringw((*i)->name.c_str()), core::rect<s32>((*i)->position.X, (*i)->position.Y - (1.5f*radiusSector), 300, 50), video::SColor(255,255,255,255));
+		//if ((*i)->explored)
+		//{
+			(*i)->draw();
+			font->draw(irr::core::stringw((*i)->name.c_str()), core::rect<s32>((*i)->position.X, (*i)->position.Y - (1.5f*radiusSector), 300, 50), video::SColor(255,255,255,255));
+		//}
 	}
 
 	Entity::draw();
