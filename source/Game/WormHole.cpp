973202ea852f5b29ff06f94ef3f6fbc085011f96
Roy Springer <roy.springer@hva.nl>
2013-04-04 13:22:11 +0200

    Update wormhole

--- source/Game/WormHole.cpp
+++ source/Game/WormHole.cpp
@@ -1,9 +1,19 @@
 #include "WormHole.h"
+#include "Engine/Game.h"
 
 WormHole::WormHole(Composite* parent, unsigned int id, irr::core::vector3df position) : Entity(parent) {
-	createNode("../assets/sydney.md2");
+	const char* filePath = "../assets/Textures/Wormholes/WormHole.png";
 	this->id = id;
 	this->position = position;
+	_wormhole = Game::getSceneManager()->addBillboardSceneNode(0, irr::core::dimension2df(30.0f,30.0f) );
+	_wormhole->setMaterialFlag(EMF_LIGHTING, false);
+	_wormhole->setMaterialType(EMT_TRANSPARENT_ALPHA_CHANNEL);
+	_wormhole->setMaterialTexture(0, Game::driver->getTexture( filePath ));
+	//this->angularVelocity = irr::core::vector3df(2,0,0);
+}
+
+void WormHole::update(){
+	Entity::update();
 }
 
 void WormHole::handleMessage(unsigned int message, void* data) {
--- source/Game/WormHole.h
+++ source/Game/WormHole.h
@@ -7,10 +7,11 @@
 class WormHole : public Entity  {
 private:
 	unsigned int id;
+	irr::scene::IBillboardSceneNode* _wormhole;
 public:
 	WormHole(Composite* parent, unsigned int id, irr::core::vector3df position);
 	void handleMessage(unsigned int message, void* data = 0);
-	//void update();
+	virtual void update();
 	unsigned int getId(){ return this->id; }
 	virtual ~WormHole();
 };
