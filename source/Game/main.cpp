6ed24ba8b3465ba5a58867ac614246675d564bff
Steven Gunneweg <steven.gunneweg@hva.nl>
2013-04-07 23:55:02 +0200

    Added assets, classes and code (commented) to the project for the GalaxyMap.

--- source/Game/main.cpp
+++ source/Game/main.cpp
@@ -1,5 +1,6 @@
 #include "Engine/Game.h"
 #include "HelmSceneTest.h"
+#include "MapGenerator.h"
 #include "MainMenuScene.h"
 
 // Include memory leak detection files.
@@ -14,10 +15,20 @@ int main()
 	// Create engine
 	Game game = Game();
 
+	//Code to create the GalaxyMap
+	/*MapGenerator mapGen;
+	mapGen.init(20, 2, 5);
+	GalaxyMap* galaxyMap = mapGen.createNewMap(300, 300, 15);
+	galaxyMap->position.set(vector3df(100, 670, 0));*/
+
 	//Game::client->setupClient("145.92.13.97");
 
 	// Create test scene
 	Game::addScene(new HelmSceneTest());
+
+	//Code to add the GalaxyMap to the current scene
+	//Game::getCurrentScene()->addComponent(galaxyMap);
+	
 	
 	// Start the main loop
 	Game::run();
