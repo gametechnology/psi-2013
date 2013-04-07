a240f3074d14a7a981a504f160699eb34ae45636
Norman Vrugt <Norman.Vrugt@hva.nl>
2013-04-07 19:14:41 +0200

    Cleaning pass - removed old commented code and collision now uses appropriate boolean

--- source/Game/Shipmap.cpp
+++ source/Game/Shipmap.cpp
@@ -38,9 +38,6 @@ Shipmap::Shipmap(Composite* parent):Entity(parent)
 	
 	posX = (playerTile.x * tileSize) + offsetX;
 	posY = (playerTile.y * tileSize) + offsetY;
-	//posX = (playerTile.x * tileSize) + tileSize / 4;
-	//posY = (playerTile.y * tileSize) + tileSize / 4;
-	//pos = position2d<s32>((playerTile.x * tileSize) + offsetX, (playerTile.y * tileSize) + offsetY);
 }
 
 Shipmap::~Shipmap()
@@ -57,12 +54,6 @@ void Shipmap::draw()
 
 	Game::driver->draw2DImage(bg, rect<s32>(0,0,1060,600), rect<s32>(0,0,bg->getOriginalSize().Width,bg->getOriginalSize().Height));
 	Game::driver->draw2DImage(icon, vector2d<s32>(posX, posY));
-	//Game::driver->draw2DImage(icon, rect<s32>(posX + offsetX, posY + offsetY, posX + offsetX + iconRadius * 2, posY + offsetY + iconRadius * 2), rect<s32>(0, 0, icon->getOriginalSize().Width, icon->getOriginalSize().Height));
-
-	/*for (int i = 0; i < 12; i++)
-		for (int j = 0; j < 7; j++)
-			if (tiles[i][j] == 1)
-				Game::driver->draw2DImage(icon, rect<s32>((i * tileSize) + offsetX, (j * tileSize) + offsetY, (i * tileSize) + tileSize + offsetX, (j * tileSize) + tileSize + offsetY), rect<s32>(0, 0, icon->getOriginalSize().Width, icon->getOriginalSize().Height));*/
 }
 
 void Shipmap::update()
@@ -108,26 +99,24 @@ void Shipmap::update()
 		int topTile = (posY - offsetY) / tileSize;
 		int bottomTile = ((posY + iconRadius * 2) - offsetY) / tileSize;
 
-		bool collision = false;
-
 		if (tiles[topTile][leftTile] == 1 || tiles[bottomTile][leftTile] == 1)
 		{
-			collision = true;
+			isIntersecting = true;
 		}
 		if (tiles[topTile][rightTile] == 1 || tiles[bottomTile][rightTile] == 1)
 		{
-			collision = true;
+			isIntersecting = true;
 		}
 		if (tiles[topTile][leftTile] == 1 || tiles[topTile][rightTile] == 1)
 		{
-			collision = true;
+			isIntersecting = true;
 		}
 		if (tiles[bottomTile][leftTile] == 1 || tiles[bottomTile][rightTile] == 1)
 		{
-			collision = true;
+			isIntersecting = true;
 		}
 
-		if (collision)
+		if (isIntersecting)
 		{
 			posX = savedPosX;
 			posY = savedPosY;
