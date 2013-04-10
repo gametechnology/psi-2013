087d80d92ff9c713a716a7bc67c05652532ea43a
Bas Roding <bas.roding@gmail.com>
2013-04-10 15:29:15 +0200

    Network: Implemented ENet making server and client connection

--- include/Engine/Network.h
+++ include/Engine/Network.h
@@ -2,6 +2,7 @@
 #define NETWORK
 
 #include "Engine\INetworkListener.h"
+#include <enet\enet.h>
 #include <list>
 
 // forward declare NetworkPacket to prevent circular dependancy
@@ -10,23 +11,32 @@ class NetworkPacket;
 class Network
 {
 private:
+	const int _port;
+
 	Network();
-
+	
+	bool _isServer;
+	bool _isConnected;
 	static Network* instance;
-
 	std::list<const INetworkListener*> _listeners;
+	ENetAddress _address;
+	ENetHost* _host;
+	ENetEvent _event;
+	ENetPeer* _peer;
+
 public:
 	~Network();
 	static Network* GetInstance();
 	static bool isInitialized;
 
-	void InitializeClient();
+	void InitializeClient(const char* ipAdress);
 	void InitializeServer();
+
+	bool IsServer();
+	bool IsConnected();
 
 	void SendPacket(const NetworkPacket packet, const bool reliable = false);
 	void AddListener(const INetworkListener* listener);
-
-	bool isServer;
 };
 
 enum PacketType
--- source/Engine/Network.cpp
+++ source/Engine/Network.cpp
@@ -1,12 +1,16 @@
 #include "Engine\Network.h"
 #include "Engine\NetworkPacket.h"
+#include <iostream>
 
 bool Network::isInitialized = false;
 Network* Network::instance = 0;
 
-Network::Network()
+Network::Network() : _port(1345)
 {
-	isServer = false;
+	_isServer = false;
+
+	if (enet_initialize() != 0)
+		std::cout << "An error occurred while initializing ENet.\n";
 }
 
 Network::~Network()
@@ -25,12 +29,58 @@ Network* Network::GetInstance()
 	return instance;
 }
 
-void Network::InitializeClient()
+void Network::InitializeClient(const char* ipAdress)
 {
+	std::cout << "Initializing client at port " << _port << ".\n";
+
+	_host = enet_host_create (NULL /* create a client host */,
+            1 /* only allow 1 outgoing connection */,
+            2 /* allow up 2 channels to be used, 0 and 1 */,
+            57600 / 8 /* 56K modem with 56 Kbps downstream bandwidth */,
+            14400 / 8 /* 56K modem with 14 Kbps upstream bandwidth */);
+
+	if (_host == NULL)
+		std::cout << "An error occurred while trying to create an ENet client host.\n";
+	else
+		std::cout << "Succesfully created ENet client host.\n";
+
+	enet_address_set_host(&_address, ipAdress);
+	_address.port = 1234;
+
+	_peer = enet_host_connect(_host, &_address, 2, 0);
+
+	if (_peer == NULL)
+		std::cout << "No available peers for initiating an ENet connection.\n";
+
+	// If connect send packages
+	if (enet_host_service(_host, &_event, 1000) > 0 && _event.type == ENET_EVENT_TYPE_CONNECT)
+	{
+		_isConnected = true;
+		printf("Connection to %s:%i succeeded.\n", ipAdress, _address.port);
+	}
+	else
+	{
+		enet_peer_reset(_peer);
+		printf("Connection to %s:%i failed.\n", ipAdress, _address.port);
+	}
+
 }
 
 void Network::InitializeServer()
 {
+	std::cout << "Initializing server at port " << _port << ".\n";
+	_address.host = ENET_HOST_ANY;
+	_address.port = _port;
+
+	_host = enet_host_create(&_address, 32, 2, 0, 0);
+
+	if (_host == NULL)
+		std::cout << "An error occurred while trying to create an ENet server host.\n";
+	else
+	{
+		std::cout << "Succesfully creatinga ENet server host; server now running.\n";
+		_isServer = true;
+	}
 }
 
 void Network::SendPacket(const NetworkPacket packet, const bool reliable)
@@ -41,4 +91,14 @@ void Network::SendPacket(const NetworkPacket packet, const bool reliable)
 void Network::AddListener(const INetworkListener* listener)
 {
 	_listeners.push_back(listener);
+}
+
+bool Network::IsConnected()
+{
+	return _isConnected;
+}
+
+bool Network::IsServer()
+{
+	return _isServer;
 }
