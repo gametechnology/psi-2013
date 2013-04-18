beb443f251d27f540c97aa6cfb3624e229d9d41c
Jorn Theunissen <jorn-theunissen@hotmail.com>
2013-04-18 12:41:57 +0200

    temporary code for starting up client or server.

--- source/Game/main.cpp
+++ source/Game/main.cpp
@@ -5,6 +5,7 @@
 #include "MapGenerator.h"
 #include "MainMenuScene.h"
 #include "SectorManager.h"
+#include "NetworkInterface.h"
 
 
 // Include memory leak detection files.
@@ -26,20 +27,30 @@ int main()
 	// Create engine
 	Game game;
 
-	// Create test scene
-	//Game::addScene(new MainMenuScene());
+	
+	/////////////////////////////////////////
+	//temporary until main menu works
+	//Server 
+	Network::GetInstance()->InitializeServer(16);
+	
+	//Client 
+	//Network::GetInstance()->InitializeClient("192.168.12.89");
+	/////////////////////////////////////////
 
-	MapGenerator mapGen;
-	mapGen.init(20, 2, 5);
-	GalaxyMap* galaxyMap = mapGen.createNewMap(300, 300, 15);
-	galaxyMap->position.set(vector3df(100, 670, 0));
+	// Create test scene
+	Game::addScene(new Scene());
+
+	//MapGenerator mapGen;
+	//mapGen.init(20, 2, 5);
+	//GalaxyMap* galaxyMap = mapGen.createNewMap(300, 300, 15);
+	//galaxyMap->position.set(vector3df(100, 670, 0));
 	
 	//Game::client->setupClient("145.92.13.97");
 	//Need to create an Scene first or else it will crash, because I first delete then create scenes in SectorManager
-	Game::addScene(new Scene());
+	//Game::addScene(new Scene());
 	// Create sector manager that creates all the Scenes/Sectors
-	SectorManager sectorManager(galaxyMap);
-	sectorManager.init();
+	//SectorManager sectorManager(galaxyMap);
+	//sectorManager.init();
 
 	// Start the main loop
 	Game::run();
