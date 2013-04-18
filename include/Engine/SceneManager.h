758abae582f1500da380d29d94f70e3d09d6d23f
Stijn Groothuis <stijn.groothuis@hva.nl>
2013-04-18 14:51:03 +0200

    Added scene manager to Engine (not fully implemented)

--- include/Engine/SceneManager.h
+++ include/Engine/SceneManager.h
@@ -1,14 +1,34 @@
-#ifndef SCENEMANAGER
-#define SCENEMANAGER
+#ifndef SCENE_MANAGER
+#define SCENE_MANAGER
 
-#include "Component.h"
+#include <vector>
+#include <Engine\Scene.h>
+#include <stdlib.h>
 
-class SceneManager : public Component {
+typedef struct {
+	char* name;
+	Scene* scene;
+} NameScene;
+
+class SceneManager
+{
+	protected:
+	std::vector<NameScene> nameScenes;
+
 public:
 	SceneManager();
-	~SceneManager();
-
-	void drawAll();
+	~SceneManager(void);
+	void init();
+	void addScene(char* name,Scene* scene);
+	void removeScene(char* name);
+	void activateScene(char* name);
+	void deactivateScene(char* name);
+	NameScene getNameScene(char* name);
+	Scene* getScene(char* name);
+	void destroyScene(char* name);
+	bool exists(char* name);
 };
+
+
 
 #endif
