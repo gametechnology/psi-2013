3715846c1524b3922eb994da87485c308330b387
Steven van der Vegt <Steven.van.der.Vegt@hva.nl>
2013-04-19 22:37:24 +0100

    Opperators now with with pointers

--- source/Game/Player.cpp
+++ source/Game/Player.cpp
@@ -19,12 +19,12 @@ Player::~Player()
 {
 	Entity::~Entity();
 }
-sf::Packet& operator >>(sf::Packet& in, Player& out)
+sf::Packet& operator >>(sf::Packet& in, Player * out)
 {
-	return in >> out.Name >> out.Team;
+	return in >> out->Name >> out->Team;
 }
 
-sf::Packet& operator <<(sf::Packet& out, const Player& in)
+sf::Packet& operator <<(sf::Packet& out, const Player * in)
 {
-	return out << in.Name << in.Team;
+	return out << in->Name << in->Team;
 }
