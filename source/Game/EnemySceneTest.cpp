c30f1ce58a0ca13833eda8b3c339214e8aec6a28
Hugo Mater <Hugo.Mater@hva.nl>
2013-04-22 01:26:43 +0200

    fixed some warnings and a network fix (now actually adds enemies as a client)

--- source/Game/EnemySceneTest.cpp
+++ source/Game/EnemySceneTest.cpp
@@ -24,7 +24,7 @@ void EnemySceneTest::HandleNetworkMessage(NetworkPacket packet)
 	{
 		if(!Network::GetInstance()->IsServer())
 		{
-			int size;
+			unsigned int size;
 			array<int> enemyIds = array<int>();
 			array<int> enemyType = array<int>();
 			array<vector3df> enemyPositions = array<vector3df>();
@@ -38,7 +38,7 @@ void EnemySceneTest::HandleNetworkMessage(NetworkPacket packet)
 
 			if(size != _enemyList.size())
 			{
-				for(int i = 0; i < _enemyList.size(); i++)
+				for(unsigned int i = 0; i < _enemyList.size(); i++)
 				{
 					if(!enemyIds.binary_search(_enemyList[i]->getId()))
 					{
@@ -48,34 +48,33 @@ void EnemySceneTest::HandleNetworkMessage(NetworkPacket packet)
 
 				if(size > _enemyList.size())
 				{
-					for(int i = 0; i < size; i++)
+					for(unsigned int i = 0; i < size; i++)
 					{
-						for(int j = 0; j < _enemyList.size(); j++)
+						for(unsigned int j = 0; j < _enemyList.size(); j++)
 						{
 							if(enemyIds[i] == _enemyList[j]->getId())
 								continue;
 						}
 
-						Enemy* enemy;
 						switch((Enemy::EnemyType)enemyType[i])
 						{
 						case Enemy::ASTROID:
-							enemy = new EnemyAsteroid(enemyPositions[i], enemyVelocities[i]);
+							_enemyList.push_back(new EnemyAsteroid(enemyPositions[i], enemyVelocities[i]));
 							break;
 						case Enemy::DRONE:
-							enemy = new EnemyDrone(enemyPositions[i]);
+							_enemyList.push_back(new EnemyDrone(enemyPositions[i]));
 							break;
 						case Enemy::FIGHTER:
-							enemy = new EnemyFighter(enemyPositions[i]);
+							_enemyList.push_back(new EnemyFighter(enemyPositions[i]));
 							break;
 						}
 
-						enemy->setId(enemyIds[i]);
-						enemy->setVelocity(enemyVelocities[i]);
-						enemy->setRotation(enemyRotations[i]);
-						enemy->setHealth(enemiesHealth[i]);
+						_enemyList.getLast()->setId(enemyIds[i]);
+						_enemyList.getLast()->setVelocity(enemyVelocities[i]);
+						_enemyList.getLast()->setRotation(enemyRotations[i]);
+						_enemyList.getLast()->setHealth(enemiesHealth[i]);
 
-						_enemyList.push_back(enemy);
+						addComponent(_enemyList.getLast());
 					}
 				}
 			}
@@ -118,7 +117,7 @@ void EnemySceneTest::update()
 			array<bool> enemiesAreAlive = array<bool>();
 			array<int> enemiesHealth = array<int>();
 
-			for(int i = 0; i < EnemySceneTest::_enemyList.size(); i++)
+			for(unsigned int i = 0; i < EnemySceneTest::_enemyList.size(); i++)
 			{
 				enemyIds.push_back(_enemyList[i]->getId());
 				enemyType.push_back(_enemyList[i]->getType());
@@ -144,12 +143,12 @@ void EnemySceneTest::createEnemies()
 {
 	this->_enemyList = array<Enemy*>();
 
-	for(int i = 0; i < 20; i++)
+	for(unsigned int i = 0; i < 20; i++)
 	{
-		_enemyList.push_back(new EnemyDrone(irr::core::vector3df(0,0,RandomGenerator::getRandomInt(10, 1))));
+		_enemyList.push_back(new EnemyDrone(irr::core::vector3df(0,0,RandomGenerator::getRandomInt(100, 1))));
 	}
 
-	for(int j = 0; j < _enemyList.size(); j++)
+	for(unsigned int j = 0; j < _enemyList.size(); j++)
 	{
 		addComponent(_enemyList[j]);
 	}
