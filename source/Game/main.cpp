60408097e480f93efd48f26d8cf68270fd633912
Simon Karman <simon.karman@hva.nl>
2013-04-25 12:06:19 +0200

    Clean up main file because it was full of old comments.

--- source/Game/main.cpp
+++ source/Game/main.cpp
@@ -28,35 +28,11 @@ int main()
 	// Create engine
 	Game game;
 
-	
-	/////////////////////////////////////////
-	//temporary until main menu works
-	//Server 
-	//Network::GetInstance()->InitializeServer(16);
-	
-	//Client 
-	//Network::GetInstance()->InitializeClient("192.168.12.89");
-	/////////////////////////////////////////
+	//Add the MainMenu
+	Game::addScene(new MainMenuScene());
 
-	// Create test scene
-	//Game::addScene(new Scene());
-	Game::addScene(new MainMenuScene());
-			//Silvan's test scene for HUD stuff. Enable it if you want to see an epic healthbar in an empty scene
-	//EmptyTestScene* emptyScene = new EmptyTestScene();
-	//Game::addScene(emptyScene);
-	/*
-	Game::addScene(new Scene());
-	MapGenerator mapGen;
-	mapGen.init(20, 2, 5);
-	GalaxyMap* galaxyMap = mapGen.createNewMap(300, 300, 15);
-	galaxyMap->position.set(vector3df(100, 670, 0));
-	
-	//Game::client->setupClient("145.92.13.97");
-	//Need to create an Scene first or else it will crash, because I first delete then create scenes in SectorManager
-	Game::addScene(new MainMenuScene());
-	// Create sector manager that creates all the Scenes/Sectors
-	SectorManager sectorManager(galaxyMap);
-	sectorManager.init();*/
+	//Epic healthbar in an empty scene
+	Game::addScene(new EmptyTestScene());
 
 	// Start the main loop
 	Game::run();
