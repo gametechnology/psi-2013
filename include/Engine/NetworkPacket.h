dfaa918afe8a3bbf264ba49067722c7329009b49
Hugo Mater <Hugo.Mater@hva.nl>
2013-05-05 21:46:53 +0200

    changed the operators to std::vector instead of irr array

--- include/Engine/NetworkPacket.h
+++ include/Engine/NetworkPacket.h
@@ -24,20 +24,20 @@ public:
 	friend sf::Packet& operator <<(sf::Packet& out, const irr::core::vector2df& in);
 	friend sf::Packet& operator >>(sf::Packet& in, irr::core::vector2df& out);
 
-	friend sf::Packet& operator <<(sf::Packet& out, const irr::core::array<irr::core::vector3df>& in);
-	friend sf::Packet& operator >>(sf::Packet& in, irr::core::array<irr::core::vector3df>& out);
+	friend sf::Packet& operator <<(sf::Packet& out, const std::vector<irr::core::vector3df>& in);
+	friend sf::Packet& operator >>(sf::Packet& in, std::vector<irr::core::vector3df>& out);
 
-	friend sf::Packet& operator <<(sf::Packet& out, const irr::core::array<irr::core::vector2df>& in);
-	friend sf::Packet& operator >>(sf::Packet& in, irr::core::array<irr::core::vector2df>& out);
+	friend sf::Packet& operator <<(sf::Packet& out, const std::vector<irr::core::vector2df>& in);
+	friend sf::Packet& operator >>(sf::Packet& in, std::vector<irr::core::vector2df>& out);
 
-	friend sf::Packet& operator <<(sf::Packet& out, const irr::core::array<bool>& in);
-	friend sf::Packet& operator >>(sf::Packet& in, irr::core::array<bool>& out);
+	friend sf::Packet& operator <<(sf::Packet& out, const std::vector<bool>& in);
+	friend sf::Packet& operator >>(sf::Packet& in, std::vector<bool>& out);
 
-	friend sf::Packet& operator <<(sf::Packet& out, const irr::core::array<int>& in);
-	friend sf::Packet& operator >>(sf::Packet& in, irr::core::array<int>& out);
+	friend sf::Packet& operator <<(sf::Packet& out, const std::vector<int>& in);
+	friend sf::Packet& operator >>(sf::Packet& in, std::vector<int>& out);
 
-	friend sf::Packet& operator <<(sf::Packet& out, const irr::core::array<float>& in);
-	friend sf::Packet& operator >>(sf::Packet& in, irr::core::array<float>& out);
+	friend sf::Packet& operator <<(sf::Packet& out, const std::vector<float>& in);
+	friend sf::Packet& operator >>(sf::Packet& in, std::vector<float>& out);
 
 private:
 	PacketType _type;
