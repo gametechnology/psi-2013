b6f688387805f60389292ed6d0831b6bed2bd4b2
Johan Terink <johan.terink@hva.nl>
2013-05-10 01:27:23 +0200

    Added some enemies; added FPS Camera;

--- source/Game/CollisionTestScene.cpp
+++ source/Game/CollisionTestScene.cpp
@@ -1,7 +1,13 @@
 #include "CollisionTestScene.h"
 
-CollisionTestScene::CollisionTestScene() : Scene() {
-
+CollisionTestScene::CollisionTestScene(void) : Scene() 
+{
+	//Network::GetInstance()->AddListener(SERVER_ENEMY, this);
+	this->_enemyList = vector<Enemy*>();
+	_player1 = new EnemyPlayer(irr::core::vector3df(0,1,0),irr::core::vector3df(0,0,0));
+	addChild(_player1);
+	_player2 = new EnemyPlayer(irr::core::vector3df(20,1,0),irr::core::vector3df(-1,0,0));
+	addChild(_player2);
 }
 
 void CollisionTestScene::onAdd() 
@@ -10,20 +16,52 @@ void CollisionTestScene::onAdd()
 
 void CollisionTestScene::init() {
 	// Create model entity
-	_player = new Player();
+	/*_player = new Player();
 	addChild(_player);
 	_player->handleMessage(DAMAGE, new int(789));	// Send a message to player and components.
 
 	// Create camera entity
 	_camera = new Camera();
 	_camera->transform->velocity->Z -= (float)0.001;
-	addChild(_camera);
+	addChild(_camera);*/
 
+	this->game->device->getSceneManager()->addCameraSceneNodeFPS();
+	createTestEnemies();
 	Scene::init();
 }
 
 void CollisionTestScene::update() {
 	Scene::update();
+}
+
+void CollisionTestScene::createTestEnemies(){
+
+	//Create a row of EnemyDrones
+	for(int i = 0; i < 5; i++)
+	{
+		_enemyList.push_back(new EnemyDrone(irr::core::vector3df(0,0,(irr::f32)(i + (i * i)))));
+		
+		addChild(_enemyList.back());
+	}
+
+	//Create a row of EnemyFighters
+	for(int j = 2; j < 5; j++)
+	{
+		_enemyList.push_back(new EnemyFighter(irr::core::vector3df(0,(irr::f32)(j + (j * j)),0)));
+		
+		addChild(_enemyList.back());
+	}
+
+	//Create a row of Enemy Asteroids
+	for(int k = 2; k < 5; k++)
+	{
+		_enemyList.push_back(new EnemyAsteroid(irr::core::vector3df((irr::f32)(k + (k * k)),0,0), irr::core::vector3df(0,0,0.01f)));
+		
+		addChild(_enemyList.back());
+	}
+
+	_enemyList.push_back(new EnemyAsteroid(irr::core::vector3df(-10,0,0), irr::core::vector3df(0.02f,0,0)));
+	addChild(_enemyList.back());
 }
 
 
--- source/Game/CollisionTestScene.h
+++ source/Game/CollisionTestScene.h
@@ -2,26 +2,44 @@
 #define CollisionTestScene_h
 
 #include "Engine\Scene.h"
+#include "Engine\Camera.h"
 #include "Stations\Station.h"
 #include "Messages.h"
+#include "NetworkInterface.h"
 
+
+#include "Enemy.h"
+#include "EnemyPlayer.h"
+#include "EnemyAsteroid.h"
+#include "EnemyDrone.h"
+#include "EnemyFighter.h"
 #include "Player.h"
-#include "Engine/Camera.h"
 
 // Forward declare Shipmap
 class Shipmap;
 
 class CollisionTestScene : public Scene {
 public:
-	CollisionTestScene();
-	virtual ~CollisionTestScene();
+	CollisionTestScene(void);
+	virtual ~CollisionTestScene(void);
 
 	void init();
 	void onAdd();
 	void update();
+	void createTestEnemies();
+	void HandleNetworkMessage(NetworkPacket packet);
+
 private:
 	Player* _player;
 	Camera* _camera;
+	vector<Enemy*> _enemyList;
+	
+	//Visual testing with camera and light
+	//irr::scene::ILightSceneNode*	_light;
+
+	//add two players for testing
+	EnemyPlayer* _player1;
+	EnemyPlayer* _player2;
 };
 
 #endif
