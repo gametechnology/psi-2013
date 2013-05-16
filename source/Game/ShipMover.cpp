1a0a24dd1d7d30b5395173290a90077499dc69c4
Steven van der Vegt <Steven.van.der.Vegt@hva.nl>
2013-05-16 23:30:59 +0100

    delay on sending to stop flooding of packages

--- source/Game/ShipMover.cpp
+++ source/Game/ShipMover.cpp
@@ -9,7 +9,7 @@
 
 ShipMover::ShipMover(Ship* _ship) : BasicMoverComponent() {
 	this->_ship = _ship;
-	
+	lastsend = time(0);
 	maxFwdSpeed = 10.f;
 	maxBwdSpeed = -10.f;
 }
@@ -63,8 +63,12 @@ void ShipMover::update() {
 	movementPacket << entity->transform->acceleration; 
 	movementPacket << entity->transform->angularAccelaration; 
 	movementPacket << entity->transform->angularVelocity;
-
+	
 	//Send packet to server
-	if(Network::GetInstance()->IsServer())
-		Network::GetInstance()->SendServerPacket(movementPacket, false);
+	if(Network::GetInstance()->IsServer()){
+		if (lastsend + 1 < time(0) ){
+			Network::GetInstance()->SendServerPacket(movementPacket, false);
+			lastsend = time(0) ;
+		}
+	}
 }
