f6fece319b5604a5bbd0e29c9ed3fcb31edc825a
elisa pijn <elisa.pijn@hva.nl>
2013-05-22 12:59:57 +0200

    change key functions to set target

--- source/Game/CollisionTestScene.cpp
+++ source/Game/CollisionTestScene.cpp
@@ -4,8 +4,10 @@
 EnemyFighter* dummyFighter = new EnemyFighter(irr::core::vector3df(10,10,10));
 
 //Creates a dummy ship, without any extra properties
-
 Ship* playerShip = new Ship(vector3df(10,10,10), vector3df(0));
+
+int target=0; 
+
 CollisionTestScene::CollisionTestScene(void) : Scene() 
 {
 	//Create a list of the enemies
@@ -27,34 +29,39 @@ void CollisionTestScene::init() {
 	//createTestEnemies();
 	//dummyFighter->setVelocity(irr::core::vector3df(0.01f,0,0));
 	//addChild(dummyFighter);
-	addChild(_ship);
+	//addChild(_ship);
 	Scene::init();
 }
 
 void CollisionTestScene::update() {
 
-	//Using the Input Manager, different scenes are created
+	//Using the Input Manager, different targets are set
 	if(game->input->isKeyboardButtonPressed(KEY_KEY_1))
 	{
 		std::cout << "Test Laser-Enemy is activated \n";
 		createLaserTestObjects();
 	}
-	/*if(game->input->isKeyboardButtonPressed(KEY_KEY_2))
+	if(game->input->isKeyboardButtonPressed(KEY_KEY_2))
 	{
-		std::cout << "Test Bullet-Enemy is activated \n";
-		createBulletTestObjects();
+		std::cout << "Target is drone";
+		target = 1;
 	}
 	if(game->input->isKeyboardButtonPressed(KEY_KEY_3))
 	{
-		std::cout << "Test Laser-Bullet is activated \n";
-		createLaserToBullet();
-	}*/
-	/*if(game->input->isKeyboardButtonPressed(KEY_KEY_4))
+		std::cout << "Target is Fighter";
+		target = 2;
+	}
+	if(game->input->isKeyboardButtonPressed(KEY_KEY_4))
 	{
-		std::cout << "Test Laser-Ship is activated \n";
-		createLaserAndShip();
-	}*/
+		std::cout << "Target is asteroid";
+		target = 3;
+	}
 	if(game->input->isKeyboardButtonPressed(KEY_KEY_5))
+	{
+		std::cout << "Target is dummyship";
+		target = 4;
+	}
+	if(game->input->isKeyboardButtonPressed(KEY_KEY_9))
 	{
 		std::cout << "Test Remove Enemy is activated \n";
 		createRemoveEnemyTest();
@@ -68,11 +75,44 @@ void CollisionTestScene::update() {
 			//_enemyList[l]->inRangeList.push_back(dummyFighter);
 			if(_enemyList.size() >= 2)
 			{
-				if(_enemyList[0] != NULL&&_enemyList[1]!=NULL)
+				switch(target)
 				{
-					_enemyList[l]->inRangeList.push_back(_enemyList[0]); //to drone 1
-					_enemyList[l]->inRangeList.push_back(_enemyList[1]);
+					case 1
+						:
+					{
+						if(_enemyList[0] != NULL&&_enemyList[1]!=NULL)
+						{
+							_enemyList[l]->inRangeList.push_back(_enemyList[0]); //to drone 1
+							_enemyList[l]->inRangeList.push_back(_enemyList[1]);
+						}
+					}break;
+					case 2:
+					{
+						if(_enemyList[2] != NULL&&_enemyList[3]!=NULL)
+						{
+							_enemyList[l]->inRangeList.push_back(_enemyList[2]); //to fighter 1
+							_enemyList[l]->inRangeList.push_back(_enemyList[3]);
+						}
+					}break;
+					case 3:
+					{
+						if(_enemyList[4] != NULL&&_enemyList[5]!=NULL)
+						{
+							_enemyList[l]->inRangeList.push_back(_enemyList[4]); //to asteroid 1
+							_enemyList[l]->inRangeList.push_back(_enemyList[5]);
+						}
+					}break;
+					case 4:
+					{
+						if(_enemyList[6] != NULL)
+						{
+							_enemyList[l]->inRangeList.push_back(_enemyList[6]); //to dummyship
+						}
+					}break;
+
+
 				}
+				
 			}
 			
 			//_enemyList[l]->inRangeList.push_back(_enemyList[7]); //to asteroid 1
@@ -94,20 +134,21 @@ void CollisionTestScene::createLaserTestObjects(){
 	}
 
 	//Create a row of EnemyFighters
-	for(int j = 0; j < 1; j++)
+	for(int j = 0; j < 2; j++)
 	{
 		_enemyList.push_back(new EnemyFighter(irr::core::vector3df(20,0,(irr::f32)(j + (j * j)))));
 		addChild(_enemyList.back());
 	}
 
 	//Create a row of Enemy Asteroids
-	for(int k = 0; k < 1; k++)
+	for(int k = 0; k < 2; k++)
 	{
 		_enemyList.push_back(new EnemyAsteroid(irr::core::vector3df(-20,0,(irr::f32)(k + (k * k))), irr::core::vector3df(0,0,0.01f)));
 		
 		addChild(_enemyList.back());
 	}
 
+	//Create an enemy ship
 	for(int l=0; l<1; l++)
 	{
 		_enemyList.push_back(new DummyShip(irr::core::vector3df(10,10,10)));
