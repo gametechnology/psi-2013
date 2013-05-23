47c4eb680f0539b4a3d76e4093a3f10b73bc8cee
Diego Arize <Diego.Barbosa.Arize.Santos@hva.nl>
2013-05-23 23:58:50 +0200

    HudComposite Health/Energy Bar position fixed

--- source/Game/HudComposite.cpp
+++ source/Game/HudComposite.cpp
@@ -4,8 +4,8 @@
 HudComposite::HudComposite( int* health, int* power, rect<s32> buttonPos, std::string* helpText) : Entity()
 {
 	
-	healthBar = new HealthBar( irr::core::vector2df(50, 110), health, irr::core::vector2df(20, 6), 5, irr::video::SColor(255, 255, 100,100));
-	energyBar = new HealthBar( irr::core::vector2df(50, 160), power, irr::core::vector2df(20, 6), 5, irr::video::SColor(255, 100, 100,255));
+	healthBar = new HealthBar( irr::core::vector2df(700, 80), health, irr::core::vector2df(20, 6), 5, irr::video::SColor(255, 255, 100,100));
+	energyBar = new HealthBar( irr::core::vector2df(700, 130), power, irr::core::vector2df(20, 6), 5, irr::video::SColor(255, 100, 100,255));
 	backButton = new BackButton(buttonPos);
 	//hudHelpText = new HudHelpText(helpText, irr::core::rect<s32>(50, 500, 90, 520));
 	//minimap
