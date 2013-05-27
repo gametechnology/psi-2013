bc0527627bb78408bed5524bb35a257fcfad5474
Simon Karman <simon.karman@hva.nl>
2013-05-27 11:37:08 +0200

    Fixed player Name not being initialized in the second constructor

--- source/Game/Player.cpp
+++ source/Game/Player.cpp
@@ -6,9 +6,13 @@ Player::Player() : Entity()
 	Name = new wchar_t[500];
 	_station = 999;
 }
+
 // TODO Check merge
 Player::Player( wchar_t *namec, enet_uint32 ipadressc,int teamc ) : Entity()
 {
+	Name = new wchar_t[500];
+	_station = 999;
+
 	wcsncpy(Name, namec, wcslen(namec));
 	Name[wcslen(namec)] = 0;
 	Ipadres = ipadressc;
