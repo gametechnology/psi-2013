3d76c1bfd63fe1996c72679fced8f88ea597149f
Diego Arize <Diego.Barbosa.Arize.Santos@hva.nl>
2013-06-06 13:06:03 +0200

    ShipHealthComponent test with the EmptyTestScene

--- source/Game/EmptyTestScene.cpp
+++ source/Game/EmptyTestScene.cpp
@@ -1,5 +1,6 @@
 #include "EmptyTestScene.h"
 #include <iostream>
+#include <stdlib.h>
 
 EmptyTestScene::EmptyTestScene(void){
 
@@ -12,25 +13,32 @@ EmptyTestScene::~EmptyTestScene(void) {
 }
 
 void EmptyTestScene::onAdd(){
-	a = 100;
-	b = 25;
+	a = 0;
+	//b = 25;
 
 	//hud = new HudComposite( &a, &b, rect<s32>(10,240,110,240 + 32));
 	//addChild(hud);
+	addChild(ship);
 }
 
 void EmptyTestScene::init(){
 	
 
 	Entity::init();
+	ship = new Ship(irr::core::vector3df(0,0,0), irr::core::vector3df(0,0,0));
 }
 
 void EmptyTestScene::update(){
+	/*
 	if(game->input->isKeyboardButtonDown(KEY_KEY_W)){
 		a -= 1;
 	}
+	*/
 	if(game->input->isKeyboardButtonDown(KEY_KEY_S)){
-		a += 1;
+		a = rand() % 100;
+		std::cout << "Give " << a << " of damage to the ship" << endl;
+		ship->shipHealthComponent->assignDamage(a);
 	}
+	
 	
 }
