4b57e284e56ebefff651facef0acf4080a04c8d0
Hugo Mater <Hugo.Mater@hva.nl>
2013-06-09 17:00:16 +0200

    added a special proxy ship for the server so it can hold more data which it needs

--- source/Game/ProxyShip.cpp
+++ source/Game/ProxyShip.cpp
@@ -1,37 +1,114 @@
 #include "ProxyShip.h"
 
-ProxyShip::ProxyShip(int teamID)
+
+////////////////////////////////////////////
+//Client proxyship below
+///////////////////////////////////////////
+
+ClientProxyShip::ClientProxyShip(int teamID)
 {
 	this->_teamId = teamID;
 }
 
-ProxyShip::~ProxyShip()
+ClientProxyShip::~ClientProxyShip()
 {
 	Entity::~Entity();
 }
 
-void ProxyShip::init()
+void ClientProxyShip::init()
 {
-
+	Entity::init();
 }
 
-void ProxyShip::onAdd()
+void ClientProxyShip::onAdd()
 {
 	IrrlichtNode *model = new IrrlichtNode( irr::io::path("../assets/Models/myship.obj"));
 	addChild(model);
+
+	Entity::onAdd();
 }
 
-void ProxyShip::update()
+void ClientProxyShip::update()
 {
 
 }
-int ProxyShip::getTeamId()
+int ClientProxyShip::getTeamId()
 {
 	return this->_teamId;
 }
 
-void ProxyShip::HandleNetworkMessage(NetworkPacket packet)
+void ClientProxyShip::HandleNetworkMessage(NetworkPacket packet)
 {
 	
 }
+
+
+///////////////////////////////////////////////////////
+//Server proxyship below
+///////////////////////////////////////////////////////
+
+
+ServerProxyShip::ServerProxyShip(int teamId)
+{
+	this->_teamId = teamId;
+	this->_stationsInUse = std::map<StationType, bool>();
+}
+
+ServerProxyShip::~ServerProxyShip()
+{
+
+}
+
+int ServerProxyShip::getTeamId()
+{
+	return this->_teamId;
+}
+
+void ServerProxyShip::HandleNetworkMessage(NetworkPacket packet)
+{
+
+}
+
+bool ServerProxyShip::StationInUse(StationType type)
+{
+	return this->_stationsInUse.find(type)->second;
+}
+
+int ServerProxyShip::getHealth()
+{
+	return this->_healthComponent.health;
+}
+
+void ServerProxyShip::init()
+{
+	Entity::init();
+}
+
+void ServerProxyShip::onAdd()
+{
+	IrrlichtNode *model = new IrrlichtNode( irr::io::path("../assets/Models/myship.obj"));
+	addChild(model);
+
+	Entity::onAdd();
+}
+
+void ServerProxyShip::update()
+{
+	Entity::update();
+}
+
+void ServerProxyShip::freeStation(StationType type)
+{
+	this->_stationsInUse.find(type)->second = false;
+}
+
+bool ServerProxyShip::enterStation(StationType type)
+{
+	if(this->StationInUse(type))
+		return false;
+
+	this->_stationsInUse.find(type)->second = true;
+	return true;
+}
+
 
--- source/Game/ProxyShip.h
+++ source/Game/ProxyShip.h
@@ -5,13 +5,15 @@
 #include "NetworkInterface.h"
 #include "Engine/IrrlichtNode.h"
 #include "BasicMoverComponent.h"
-#include "ShipInterFace.h"
+#include "ShipInterface.h"
+#include "Stations\Station.h"
+#include "HealthComponent.h"
 
-class ProxyShip :  public ShipInterFace, public INetworkListener
+class ClientProxyShip :  public ShipInterface, public INetworkListener
 {
 public:
-	ProxyShip(int teamId);
-	~ProxyShip();
+	ClientProxyShip(int teamId);
+	~ClientProxyShip();
 
 	int getTeamId();
 	void HandleNetworkMessage(NetworkPacket packet);
@@ -22,5 +24,34 @@ public:
 	
 private:
 	int _teamId;
+};
+
+class ServerProxyShip : public ShipInterface, public INetworkListener
+{
+
+public:
+	ServerProxyShip(int teamId);
+	~ServerProxyShip();
+
+	int getTeamId();
+	void HandleNetworkMessage(NetworkPacket packet);
+
+	bool StationInUse(StationType type);
+	int getHealth();
+
+	void init();
+	void onAdd();
+	void update();
+
+private:
+
+	void freeStation(StationType type);
+
+	//returns false when the station is already in use
+	bool enterStation(StationType type);
+
+	int _teamId;
+	std::map<StationType, bool> _stationsInUse;
+	HealthComponent _healthComponent;
 };
 #endif
