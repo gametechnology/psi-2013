a144db48b32a1c6e19da43a4df57ce05e27331b7
Poly Nikolaou <polyxeni.nikolaou@hva.nl>
2013-06-10 13:10:36 +0200

    didnt test it yet, create basic function for distinction Homebase (work with Danny)

--- source/Game/Ship.h
+++ source/Game/Ship.h
@@ -85,6 +85,9 @@ public:
 
 	void notifyIShipListeners(ShipMessage message);
 
+	void foundEnemyBase();
+	void backAtOwnBase();
+
 private:
 	std::list<IShipListener*> listeners;
 
@@ -94,6 +97,8 @@ private:
 
 	stringw varToString(stringw str1, float var, stringw str2);
 	stringw varToString(stringw str1, float var);
+	bool _foundEnemyBase;
+	bool _backAtOwnBase;
 
 	void setInertiaMatrix(float h, float w, float d, float m);
 };
