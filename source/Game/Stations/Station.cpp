a01c26f3d648c5abc7db45ac2b541e238cb12ab1
Diego Arize <Diego.Barbosa.Arize.Santos@hva.nl>
2013-06-13 11:20:15 +0200

    Disabling the increase/decrease health randomly

--- source/Game/Stations/Station.cpp
+++ source/Game/Stations/Station.cpp
@@ -155,7 +155,7 @@ void Station::update()
 		_powerComponent->power -= 1;
 	// End test code for testing energy of a station.
 
-	updateHealth();
+	//updateHealth();
 
 	if (game->input->isKeyboardButtonDown(KEY_ESCAPE)){
 		stringObj->setVisible(false);
